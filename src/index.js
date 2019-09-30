d180673018d467e50c69ca2f9aa77e86c245051c
Lenocka <lvutina.en@gmail.com>
2019-09-30 14:03:40 +0300

    feat: done morse decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+    let numberOfWords=expr.length / 10;
+    let binaryWordsArray =[];
+    let j=0;
+    //create array of binary numbers devided into words
+    for(let i=0; i<numberOfWords;i++){
+        binaryWordsArray[i]=expr.slice(j,j+10); 
+        j=j+10;
+        if (binaryWordsArray[i][1]!='*'){
+        let positionOfOne=binaryWordsArray[i].indexOf(1);
+        binaryWordsArray[i]=binaryWordsArray[i].slice(positionOfOne);
+        } else binaryWordsArray[i]=0;
+        //change array of numbers into morse code
+        let morseWord="";
+        
+        for(let x=0; x<binaryWordsArray[i].length; x=x+2 ){
+            
+            if (binaryWordsArray[i].slice(x,x+2)=="10"){
+                morseWord=morseWord+".";
+            }
+            else if(binaryWordsArray[i].slice(x,x+2)=="11"){
+                morseWord=morseWord+"-";
+            }
+            else morseWord=morseWord+" ";
+
+        }
+        binaryWordsArray[i]=morseWord;
+    }
+    let uncoded="";
+    for (let i=0; i<binaryWordsArray.length; i++){
+        let b=binaryWordsArray[i];
+        
+       if (binaryWordsArray[i]!=0) {uncoded=uncoded+MORSE_TABLE[b]}
+       else uncoded=uncoded+" ";
+    }
+
+    return uncoded;
     // write your solution here
 }
+
+
 
 module.exports = {
     decode
